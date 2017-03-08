c0cbdc57f58fdf24e17f7894ea3251351bc8a28f
wanglin <wanglin@jusontech.com>
2017-03-08 19:43:53 +0800

    add game.h start.c

---
+++ game.h
@@ -0,0 +1,69 @@
+#ifndef GAME_H
+#define GAME_H
+typedef unsigned int  uint;
+
+Card poker[52];
+
+enum Suit {
+	hearts,
+	spades,
+	diamonds,
+	clubs
+};
+
+typedef struct{
+	uint suit:2;
+	uint point:4;
+}Card;
+
+typedef struct{
+	char name[20];
+	Card handcards[3];
+	int type;
+}Player;
+
+enum Type{
+	SPECIAL = 1,
+	SCATTERED,
+	PAIR,
+	STRAIGHT,
+	FLUSH,
+	STRAIGHT_FLUSH,
+	SET
+};
+
+#define _HANDCARDS_TYPE  { \
+	"SPECIAL", \
+	"SCATTERED", \
+	"PAIR", \
+	"STRAIGHT", \
+	"FLUSH",  \
+	"STRAIGHT FLUSH", \
+	"SET" \..
+	};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ start.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "game.h"
+int main(){
+	int people_num;
+	Card  poker[52];
+	peopole_num = init_game(poker);
+	switch (people_num){
+
+	}
+	return 0;
+}
